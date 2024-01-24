a22802dfe22194d4f9ea54804ad4b8aaf5af08ec
Juan Diego MARTÍNEZ <juandiego.martinez@imaweb.com>
2024-01-24 08:13:53 +0100

    Added unit tests to Result type

--- test/Functional.Tests/Result/ResultTests.cs
+++ test/Functional.Tests/Result/ResultTests.cs
@@ -1,6 +1,6 @@
-using FluentAssertions;
+﻿using FluentAssertions;
 
-namespace Functional.Tests.Option;
+namespace Functional.Tests;
 
 public class ResultTests
 {
@@ -15,5 +15,55 @@ public class ResultTests
         var result = Result.Success(new TestClass());
 
         result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Can_create_result_failure_from_error()
+    {
+        var result = Result<TestClass>.Failure(new Error("code", "message"));
+
+        result.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Throw_exception_if_create_failure_with_no_error()
+    {
+        var action = () => { var result = Result<TestClass>.Failure(Error.None); };
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Can_create_a_result_from_valid_value()
+    {
+        var test = new TestClass();
+        Result<TestClass> result = test;
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(test);
+    }
+
+    [Fact]
+    public void Can_create_a_result_from_valid_error()
+    {
+        var error = new Error("test", "error");
+        Result<TestClass> result = error;
+
+        result.IsFailure.Should().BeTrue();
+        result.Errors.Count().Should().Be(1);
+    }
+
+    [Fact]
+    public void Can_create_a_result_failure_from_error_list()
+    {
+        var errors = new List<Error>
+        {
+            new("1", "error 1"),
+            new("2", "error 2"),
+        };
+        var result = Result<TestClass>.Failure(errors);
+
+        result.IsFailure.Should().BeTrue();
+        result.Errors.Count().Should().Be(errors.Count);
     }
 }
