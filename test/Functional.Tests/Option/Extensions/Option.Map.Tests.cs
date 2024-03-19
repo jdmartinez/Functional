bb0837e9f322dd1fce3a73e99ec81b8968541aeb
Juan Diego MARTÍNEZ <juandiego.martinez@nextlane.com>
2024-03-19 08:07:05 +0100

    Added Option.Bind tests methods

---
+++ test/Functional.Tests/Option/Extensions/Option.Map.Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace Functional.Tests;
+
+public partial class OptionExtensionsTests
+{
+    [Fact]
+    public void Map_WhenOptionHasValue_ShouldApplySelectorAndReturnNewOption()
+    {
+        var testValue = "test";
+        var option = Option<TestClass>.From(new TestClass());
+        Func<TestClass, string> selector = _ => testValue;
+
+        var result = option.Map(selector);
+
+        result.Should().BeOfType<Option<string>>();
+        result.HasValue.Should().BeTrue();
+        result.Value.Should().Be(testValue);
+    }
+}
