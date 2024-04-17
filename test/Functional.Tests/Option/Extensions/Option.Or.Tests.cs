6ced50a1cd65f5e3e16c842b239d1b5a56524ab2
Juan Diego MARTÍNEZ <juandiego.martinez@nextlane.com>
2024-04-17 15:03:19 +0200

    Added more Option extension method unit tests

---
+++ test/Functional.Tests/Option/Extensions/Option.Or.Tests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+
+namespace Functional.Tests;
+
+public partial class OptionExtensionsTests
+{
+    [Fact]
+    public void Or_WhenOptionHasValue_Should_ReturnValue()
+    {
+        var test = new TestClass();
+        var fallback = new TestClass();
+        var option = Option<TestClass>.From(test);
+        var result = option.Or(fallback);
+
+        result.Value.Should().Be(test);
+    }
+
+    [Fact]
+    public void Or_WhenOptionNone_Should_ReturnsFallback()
+    {
+        var fallback = new TestClass();
+        var option = Option<TestClass>.From(default!);
+        var result = option.Or(fallback);
+
+        result.HasValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Or_WithFunction_WhenOptionHasValue_Should_ReturnValue()
+    {
+        var test = new TestClass();
+        var option = Option<TestClass>.From(test);
+        var result = option.Or(() => new TestClass());
+
+        result.Value.Should().Be(test);
+    }
+
+    [Fact]
+    public void Or_WithFunction_WhenOptionNone_Should_ReturnsFallback()
+    {
+       var option = Option<TestClass>.From(default!);
+        var result = option.Or(() => new TestClass());
+
+        result.HasValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Or_WithOptionFunction_WhenOptionHasValue_Should_ReturnValue()
+    {
+        var test = new TestClass();
+        var option = Option<TestClass>.From(test);
+        var result = option.Or(() => Option<TestClass>.From(new TestClass()));
+
+        result.Value.Should().Be(test);
+    }
+
+    [Fact]
+    public void Or_WithOptionFunction_WhenOptionNone_Should_ReturnsFallback()
+    {
+        var option = Option<TestClass>.From(default!);
+        var result = option.Or(() => Option<TestClass>.From(new TestClass()));
+
+        result.HasValue.Should().BeTrue();
+    }
+}
