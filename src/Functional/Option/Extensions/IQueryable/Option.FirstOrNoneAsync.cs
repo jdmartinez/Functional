15afb2054e040be6171a5e27b49662c013013a5e
Juan Diego Martínez <lmerrin@gmail.com>
2024-10-26 19:36:18 +0200

    Added Option IQueryable extensions

---
+++ src/Functional/Option/Extensions/IQueryable/Option.FirstOrNoneAsync.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Functional;
+
+public static partial class OptionExtensions
+{
+    public static async Task<Option<T>> FirstOrNoneAsync<T>(this IQueryable<T> source, CancellationToken cancellationToken = default)
+        => await source.FirstOrDefaultAsync(cancellationToken);
+
+    public static async Task<Option<T>> FirstOrNoneAsync<T>(
+        this IQueryable<T> source,
+        Expression<Func<T, bool>> predicate, 
+        CancellationToken cancellationToken = default)
+        => await source.FirstOrDefaultAsync(predicate, cancellationToken);
+
+}
---
+++ test/Functional.Tests/Option/Extensions/IQueryable/TestContext.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Functional.Tests;
+
+internal class Person
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+}
+
+internal class TestContext : DbContext
+{
+    public DbSet<Person> People => Set<Person>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase($"test-{Guid.NewGuid()}");
+    }
+}
