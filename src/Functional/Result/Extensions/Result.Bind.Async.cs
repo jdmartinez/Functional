ced24c57dd1a2160ac7ebf100d69c9221c2d8922
Juan Diego Martínez <lmerrin@gmail.com>
2024-12-01 11:44:41 +0100

    Added asynchronous Bind method

---
+++ src/Functional/Result/Extensions/Result.Bind.Async.cs
@@ -0,0 +1,13 @@
+﻿namespace Functional;
+
+public static partial class ResultExtensions
+{
+    public static async Task<Result<TReturn>> Bind<T, TReturn>(this Task<Result<T>> resultTask, Func<T, Task<Result<TReturn>>> func)
+    {
+        var result = await resultTask;
+        
+        if (result.IsFailure) return Result.Failure<TReturn>(result.Error);
+        
+        return await func(result.Value);
+    }
+}
---
+++ test/Functional.Tests/Result/Extensions/Result.Bind.Async.Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace Functional.Tests;
+
+public partial class ResultExtensionsTests
+{
+    [Fact]
+    public async Task BindAsync_ShouldReturnSuccess_WhenResultIsSuccess()
+    {
+        // Arrange
+        var resultTask = Result.Success(1).AsTask();
+
+        // Act
+        var bindResult = await resultTask.Bind(ToStringAsync);
+
+        // Assert
+        bindResult.Should().BeOfType<Result<string>>();
+        bindResult.IsSuccess.Should().BeTrue();
+        bindResult.Value.Should().Be("1");
+    }
+
+    [Fact]
+    public async Task BindAsync_ShouldReturnFailure_WhenResultIsFailure()
+    {
+        // Arrange
+        var error = new Error("test", "test");
+        var resultTask = Result.Failure<int>(error).AsTask();
+
+        // Act
+        var bindResult = await resultTask.Bind(ToStringAsync);
+
+        // Assert
+        bindResult.Should().BeOfType<Result<string>>();
+        bindResult.IsFailure.Should().BeTrue();
+        bindResult.Error.Should().Be(error);
+    }
+
+    private static Task<Result<string>> ToStringAsync(int value) => Result.Success(value.ToString()).AsTask();
+}
